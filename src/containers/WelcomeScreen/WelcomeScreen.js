1096dfda274505863c6291f2baf2c369c52dcbd5
Ross Jones <rosspjones13@gmail.com>
2020-01-19 15:44:56 -0500

    made text editable for space testing and changed particles to snowfall

--- src/containers/WelcomeScreen/WelcomeScreen.js
+++ src/containers/WelcomeScreen/WelcomeScreen.js
@@ -18,7 +18,7 @@ class WelcomeScreen extends Component {
               params={{
                 "particles": {
                     "number": {
-                        "value": 100,
+                        "value": 200,
                         "density": {
                           "enable": true,
                           "value_area": 1500
@@ -32,8 +32,12 @@ class WelcomeScreen extends Component {
                       "value": "0.8",
                     },
                     "line_linked": {
-                      // "enable": false,
+                      "enable": false,
                       "opacity": "0.8"
+                    },
+                    "move": {
+                      "direction": "bottom",
+                      "out_mode": "out"
                     }
                 },
                 "interactivity": {
@@ -78,7 +82,7 @@ class WelcomeScreen extends Component {
               <img className="learn-profile-photo" src="/images/blank-profile.jpg" alt="Heidi Agostini-Broadt"/>
               <h2 className="learn-profile-name">Heidi Agostini, Ph.D., PMP</h2>
               <h2 className="learn-profile-title">President</h2>
-              <p className="learn-profile-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit ut aliquam purus sit amet luctus. Massa tincidunt dui ut ornare.</p>
+              <p contentEditable={true} className="learn-profile-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit ut aliquam purus sit amet luctus. Massa tincidunt dui ut ornare.</p>
             </div>
 
             {/* <div className="learn-profile-card">
@@ -95,7 +99,7 @@ class WelcomeScreen extends Component {
           </div>
 
           <div className="learn-paragraph-container">
-            <p className="learn-about-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p contentEditable={true} className="learn-about-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <div className="learn-about-photo-container">
               <img className="learn-about-photo" src="/images/ConferenceStock.jpg" alt="stock conference"/>
             </div>
@@ -106,32 +110,32 @@ class WelcomeScreen extends Component {
               
               <div className="learn-list-item-container">
                 <img className="learn-list-icon" src="/images/eco-24px.svg" alt="conference icon"/>
-                <li className="learn-list-item">Duis at consectetur lorem donec massa sapien faucibus et molestie</li>
+                <li contentEditable={true} className="learn-list-item">Duis at consectetur lorem donec massa sapien faucibus et molestie</li>
               </div>
 
               <div className="learn-list-item-container">
                 <img className="learn-list-icon" src="/images/eco-24px.svg" alt="schedule icon"/>
-                <li className="learn-list-item">Id porta nibh venenatis cras sed felis eget velit aliquet</li>
+                <li contentEditable={true} className="learn-list-item">Id porta nibh venenatis cras sed felis eget velit aliquet</li>
               </div>
 
               <div className="learn-list-item-container">
                 <img className="learn-list-icon" src="/images/eco-24px.svg" alt="collaboration icon"/>
-                <li className="learn-list-item">Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae</li>
+                <li contentEditable={true} className="learn-list-item">Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae</li>
               </div>
 
               <div className="learn-list-item-container">
                 <img className="learn-list-icon" src="/images/eco-24px.svg" alt="todo-list icon"/>
-                <li className="learn-list-item">Gravida arcu ac tortor dignissim convallis aenean et tortor at</li>
+                <li contentEditable={true} className="learn-list-item">Gravida arcu ac tortor dignissim convallis aenean et tortor at</li>
               </div>
 
               <div className="learn-list-item-container">
                 <img className="learn-list-icon" src="/images/eco-24px.svg" alt="account icon"/>
-                <li className="learn-list-item">Aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros</li>
+                <li contentEditable={true} className="learn-list-item">Aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros</li>
               </div>
 
               <div className="learn-list-item-container">
                 <img className="learn-list-icon" src="/images/eco-24px.svg" alt="goal icon"/>
-                <li className="learn-list-item">Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt</li>
+                <li contentEditable={true} className="learn-list-item">Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt</li>
               </div>
             </ul>
           </div>
