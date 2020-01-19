3dfe8d6ddd875dbf3680347f8eba995f6f7459a2
Ross Jones <rosspjones13@gmail.com>
2020-01-19 17:16:56 -0500

    logo click takes to home

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Logo extends Component {
   render() {
     return(
-      <div className="logo-container">
-        <img id="logo-tree" src="/images/TreeGreen-nobackground.png" alt="Logo Tree"/>
-        <div className="logo-name">
-          <img id="logo-sequoia" src="/images/SequoiaGold-nobackground.png" alt="Logo Sequoia"/>
-          <img id="logo-cg" src="/images/ConsultingGroupGreen-nobackground.png" alt="Logo Consulting Group"/>
+      <Link to="home">
+        <div className="logo-container">
+          <img id="logo-tree" src="/images/TreeGreen-nobackground.png" alt="Logo Tree"/>
+          <div className="logo-name">
+            <img id="logo-sequoia" src="/images/SequoiaGold-nobackground.png" alt="Logo Sequoia"/>
+            <img id="logo-cg" src="/images/ConsultingGroupGreen-nobackground.png" alt="Logo Consulting Group"/>
+          </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
